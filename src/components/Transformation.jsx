d671e21b6bc2dcf4e881ff3f6f3406d93428451f
drp33 <dan@mechanochemistry.org>
2018-05-16 15:39:21 +0100

    add anchor points

--- src/components/Transformation.jsx
+++ src/components/Transformation.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 
 const Transformation = function(props) {
-  const points = props.coords.map((xyPair) => xyPair.join(',')).join(' ');
 
-  return (
-    <polygon points={points} fill="none" stroke="black" stroke-width="0.01"/>
-  )
+  const points = props.coords.map(xyPair => xyPair.join(',')).join(' ');
+  const rect = <polygon
+    points={points}
+    fill="none"
+    stroke="black"
+    stroke-width="0.01"
+  />;
+
+  const anchors = props.coords.map((xyPair) => {
+    const anchor = <circle cx={xyPair[0]} cy={xyPair[1]} r="0.02"/>;
+    return anchor;
+  });
+
+  return [rect, ...anchors];
 }
 
 export default Transformation
